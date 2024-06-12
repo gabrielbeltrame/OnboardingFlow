521b9a95277980886569bfb6d485914b3383b885
gabriel.beltrame <gabriel.beltrame@totvs.com.br>
2024-06-12 16:10:58 -0300

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Gabriel Beltrame Silva on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
